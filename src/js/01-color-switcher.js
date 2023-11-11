63a37793a8c0be532e56e8119d9eea2039108ccd
Andrii <Popov.Andrey80@gmail.com>
2023-11-11 11:45:08 +0200

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body')
+}
+
+refs.start.addEventListener('click', onStart)
+refs.stop.addEventListener('click', onStop)
+
+let id = null;
+
+function onStart() {
+    id = setInterval((() => refs.body.style.backgroundColor = `${getRandomHexColor()}`), 1000)
+}
+
+function onStop() {
+    clearInterval(id)
+}
